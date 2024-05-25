7add31e108b59a10651207eda5a186a20c753f25
prici <prici@prici>
2024-05-25 01:52:20 -0300

    subo el proyecto

---
+++ tp4_AcsamaPricila/src/main/java/ar/edu/unju/fi/Tp4AcsamaPricilaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4AcsamaPricilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4AcsamaPricilaApplication.class, args);
+	}
+
+}
